bef72519d3a441ca7c333370cddc30d7df4e6f22
峻宇 李 <jimmy2333060@gmail.com>
2021-07-29 14:40:01 +0800

    重構

---
+++ AccountingNote/SystemAdmin/UserInfo.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UserInfo.aspx.cs" Inherits="AccountingNote.SystemAdmin.UserInfo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnLogout" runat="server" Text="Logout" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ AccountingNote/SystemAdmin/UserInfo.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AccountingNote.SystemAdmin
+{
+    public partial class UserInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!this.IsPostBack)
+            {
+                if (this.Session["UserLoginInfo"] == null)
+                {
+                    Response.Redirect("/Login.aspx");
+                    return;
+                }
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Session["UserLoginInfo"] = null;
+            Response.Redirect("/Login.aspx");
+        }
+    }
+}
